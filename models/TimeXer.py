1e57f97587ac174903c74f2fe3bd612bc1f3e91a
Rr. Diajeng A.A <diajeng1203@gmail.com>
2026-04-23 00:20:29 +0800

    feat: new dataset

--- models/TimeXer.py
+++ models/TimeXer.py
@@ -21,14 +21,14 @@ class FlattenHead(nn.Module):
         return x
 
 
-class EnEmbedding(nn.Module):
+class EnEmbedding(nn.Module): #endogenous embedding
     def __init__(self, n_vars, d_model, patch_len, dropout):
         super(EnEmbedding, self).__init__()
         # Patching
-        self.patch_len = patch_len
+        self.patch_len = patch_len #use patch embed
 
         self.value_embedding = nn.Linear(patch_len, d_model, bias=False)
-        self.glb_token = nn.Parameter(torch.randn(1, n_vars, 1, d_model))
+        self.glb_token = nn.Parameter(torch.randn(1, n_vars, 1, d_model)) # global token
         self.position_embedding = PositionalEmbedding(d_model)
 
         self.dropout = nn.Dropout(dropout)
@@ -40,13 +40,13 @@ class EnEmbedding(nn.Module):
 
         x = x.unfold(dimension=-1, size=self.patch_len, step=self.patch_len)
         x = torch.reshape(x, (x.shape[0] * x.shape[1], x.shape[2], x.shape[3]))
-        # Input encoding
+        # Input encoding, add patch and position embedding
         x = self.value_embedding(x) + self.position_embedding(x)
         x = torch.reshape(x, (-1, n_vars, x.shape[-2], x.shape[-1]))
-        x = torch.cat([x, glb], dim=2)
+        x = torch.cat([x, glb], dim=2) #concatenate global token and patch embedding
         x = torch.reshape(x, (x.shape[0] * x.shape[1], x.shape[2], x.shape[3]))
         return self.dropout(x), n_vars
-
+        #return dropout and n_vars for later use in the head of the model
 
 class Encoder(nn.Module):
     def __init__(self, layers, norm_layer=None, projection=None):
@@ -56,6 +56,7 @@ class Encoder(nn.Module):
         self.projection = projection
 
     def forward(self, x, cross, x_mask=None, cross_mask=None, tau=None, delta=None):
+        # depends how many layers we have, we will do the cross attention multiple times
         for layer in self.layers:
             x = layer(x, cross, x_mask=x_mask, cross_mask=cross_mask, tau=tau, delta=delta)
 
